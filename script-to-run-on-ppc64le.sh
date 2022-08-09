3c32b1c5c61e34939c8e8e6ca9541c5753bf728a
Mayur Waghmode <42777188+mayurwaghmode@users.noreply.github.com>
2022-08-09 14:26:30 +0530

    Update script-to-run-on-ppc64le.sh

--- script-to-run-on-ppc64le.sh
+++ script-to-run-on-ppc64le.sh
@@ -4,4 +4,6 @@ echo -e "Display the cpu details\n"
 lscpu
 echo -e "Display the kernel details\n"
 uname -a
+git clone -b multi-arch https://github.com/mayurwaghmode/power-runner-ssh.git /root/power-runner-ssh
+cd /root/power-runner-ssh
 buildah bud -f Dockerfile -t multi-arch:ppc64le .
