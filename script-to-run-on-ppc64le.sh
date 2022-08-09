efad12e4ef51188c7cc5ce3392dc97a888281c89
Mayur Waghmode <42777188+mayurwaghmode@users.noreply.github.com>
2022-08-09 16:23:43 +0530

    Update script-to-run-on-ppc64le.sh

--- script-to-run-on-ppc64le.sh
+++ script-to-run-on-ppc64le.sh
@@ -1,4 +1,5 @@
 ARCH=$(uname -m)
+rm -rf /root/power-runner-ssh
 git clone -b multi-arch https://github.com/mayurwaghmode/power-runner-ssh.git /root/power-runner-ssh
 cd /root/power-runner-ssh
 buildah bud -f Dockerfile -t quay.io/mayurwaghmode111/gha-multi-arch:$ARCH .
